a146afbe15e8cf3d3f6c091aa6a46096e5cc15fb
Jérémy MIRRE <jeremy.mirre@skores.com>
2017-09-18 09:27:44 +0200

    Webservice cgi script improved

--- learning/webservice/cgi-bin/send.py
+++ learning/webservice/cgi-bin/send.py
@@ -1,14 +1,21 @@
-#!/usr/bin/env python2.7
+#!/usr/local/bin/python
+# -*- coding: UTF-8 -*-
+
 import subprocess
 import cgi
 import sys
-import logging
-from StringIO import StringIO
-from threading import Thread
+#import logging
+#from StringIO import StringIO
+#from threading import Thread
 import cgitb
 
+
+# Tell the browser how to render the text
+print "Content-tpe: text/plain;charset=utf-8\n\n"
+print "<html><body>"
 cgitb.enable(logdir="log")
 
+#cgi.test()
 APPLE_API_URL = "https://api.push.apple.com/3/device"
 
 form = cgi.FieldStorage(environ={'REQUEST_METHOD': 'POST'})
@@ -18,21 +25,70 @@ BUNDLE_ID = form.getvalue('bundle_id')
 PAYLOAD = form.getvalue('message')
 CERT_FILE = form.getvalue('certificat')
 
-wfile.write("<html><body><h1>POST!</h1></body></html>")
-print "Thanks your notification has been sent with following parameters :"
+if CERT_FILE == "SportyTrader":
+    CERT_FILE = '/usr/local/etc/ssl/myCertificateIphoneST.pem'
+elif CERT_FILE == "FootEnDirect":
+    CERT_FILE = '/usr/local/etc/ssl/myCertificateIphoneFED.pem'
+elif CERT_FILE == "Hello":
+    pass
+
+def get_topic(name):
+    if name == "SportyTrader":
+        name = "/usr/local/etc/ssl/myCertificateIphoneST.pem"
+    elif name == "FootEnDirect":
+        name = "/usr/local/etc/ssl/myCertificateIphoneFED.pem"
+
+    return name
+
+CERTIFICAT = get_topic(CERT_FILE)
+
+print "<p> Certificat is : %s </p>" % CERTIFICAT
+
+CURL_CMD = """/usr/local/bin/curl --verbose --data {}  --header \"apns-topic: {}\" --header \"apns-priority: 10\" --header \"method: POST\" --http2 --cert {} {}/{}""".format(PAYLOAD, BUNDLE_ID, CERTIFICAT, APPLE_API_URL, TOKEN)
+
+print "<h2>Thanks your notification has been sent with following parameters:</h2>"
 print "<p>TOKEN: %s</p>" % TOKEN
 print "<p>BUNDLE ID: %s</p>" % BUNDLE_ID
-print "<p>MESSAGE: %s</p>" % PAYLOAD
-print "<p>CERTIFICAT: %s</p>" % CERT_FILE
+print "<p>MESSAGE: \'%s\'</p>" % PAYLOAD
+print "<p>CERTIFICAT: %s</p>" % CERTIFICAT
+print "<p>COMMAND: %s</p>" % CURL_CMD
+
 
 
 
 
 with open ("../log.txt", "w") as f:
-    f.write(TOKEN)
-    f.write(BUNDLE_ID)
-    f.write(PAYLOAD)
-    f.write(CERT_FILE)
+    f.write("CURL_COMMAND: "+CURL_CMD)
+    f.write("TOKEN: "+TOKEN+"\n")
+    f.write("Bundle ID : "+ BUNDLE_ID+"\n")
+    f.write("Message : "+ PAYLOAD+"\n")
+    f.write("Certificat File : "+ CERT_FILE+"\n")
 
-print "<TITLE>CGI script output</TITLE>"
 
+
+
+#rootLogger = logging.getLogger()
+
+logfile = open('log-apache.txt', 'w')
+
+
+args = CURL_CMD.split()
+print "%s" % args
+
+f2 = open("test.test.test", "w")
+#process = subprocess.Popen(args, stdout=f2, stderr=f2, bufsize=1)
+#process = subprocess.Popen(" ".join(args), shell=True, stdout=f2, stderr=f2, bufsize=1)
+process = subprocess.Popen(" ".join(args), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, bufsize=1)
+for line in process.stdout:
+    print line
+
+
+#with process.stdout, open(logfile, 'ab') as file:
+ #       for line in iter(process.stdout.readline, b''):
+   #         print line,
+  #          file.write(line)
+#process.wait()
+
+
+
+print "</body></html>"
--- learning/webservice/new/cgi-bin/send.py
+++ learning/webservice/new/cgi-bin/send.py
@@ -1,4 +1,3 @@
-
 #!/usr/local/bin/python
 # -*- coding: UTF-8 -*-
 
@@ -19,44 +18,39 @@ cgitb.enable(logdir="log")
 #cgi.test()
 APPLE_API_URL = "https://api.push.apple.com/3/device"
 
-form = cgi.FieldStorage(environ={'REQUEST_METHOD': 'POST'})
 
-TOKEN = form.getvalue('token')
-BUNDLE_ID = form.getvalue('bundle_id')
-PAYLOAD = form.getvalue('message')
-CERT_FILE = form.getvalue('certificat')
+parsed = False
+while not parsed:
+    try:
+        form = cgi.FieldStorage(environ={'REQUEST_METHOD': 'POST'})
 
-if CERT_FILE is "SportyTrader":
-    CERT_FILE = myCertificateIphoneST.pem
-elif CERT_FILE is "FootEnDirect":
-    CERT_FILE = myCertificateIphoneFED.pem 
-elif CERT_FILE is "Hello":
+        TOKEN = form.getvalue('token')
+        BUNDLE_ID = form.getvalue('bundle_id')
+        PAYLOAD = form.getvalue('message')
+        CERT_FILE = form.getvalue('certificat')
+        parsed = True
+    except ValueError:
+        print "Incorrect value entered, please try again"
+
+
+if CERT_FILE == "Skores":
+    CERT_FILE = '/usr/local/etc/ssl/myCertificateIphoneST.pem'
+elif CERT_FILE == "FootEnDirect":
+    CERT_FILE = '/usr/local/etc/ssl/myCertificateIphoneFED.pem'
+elif CERT_FILE == "Hello":
     pass
 
+def get_topic(name):
+    if name == "SportyTrader":
+        name = "/usr/local/etc/ssl/myCertificateIphoneST.pem"
+    elif name == "FootEnDirect":
+        name = "/usr/local/etc/ssl/myCertificateIphoneFED.pem"
 
-CURL_CMD = """/usr/local/bin/curl --verbose --data "{}" --header "apns-topic: {}" \
-            --header "apns-priority: 10" --header "method: POST" --http2 \
-            --cert {} {}/{}""".format(PAYLOAD, BUNDLE_ID, CERT_FILE,
-            APPLE_API_URL, TOKEN)
+    return name
 
-print "<h2>Thanks your notification has been sent with following parameters:</h2>"
-print "<p>TOKEN: %s</p>" % TOKEN
-print "<p>BUNDLE ID: %s</p>" % BUNDLE_ID
-print "<p>MESSAGE: %s</p>" % PAYLOAD
-print "<p>CERTIFICAT: %s</p>" % CERT_FILE
-print "<p>COMMAND: %s</p>" % CURL_CMD
+CERTIFICAT = get_topic(CERT_FILE)
 
-
-
-
-
-with open ("../log.txt", "w") as f:
-    f.write("CURL_COMMAND: "+CURL_CMD)
-    f.write("TOKEN: "+TOKEN+"\n")
-    f.write("Bundle ID : "+ BUNDLE_ID+"\n")
-    f.write("Message : "+ PAYLOAD+"\n")
-    f.write("Certificat File : "+ CERT_FILE+"\n")
-
+CURL_CMD = """/usr/local/bin/curl --verbose --data {}  --header \"apns-topic: {}\" --header \"apns-priority: 10\" --header \"method: POST\" --http2 --cert {} {}/{}""".format(PAYLOAD, BUNDLE_ID, CERTIFICAT, APPLE_API_URL, TOKEN)
 
 
 
@@ -66,8 +60,33 @@ logfile = open('log-apache.txt', 'w')
 
 
 args = CURL_CMD.split()
+#print "%s" % args
+
 f2 = open("test.test.test", "w")
-process = subprocess.Popen(args, stdout=f2, stderr=f2, bufsize=1)
+#process = subprocess.Popen(args, stdout=f2, stderr=f2, bufsize=1)
+#process = subprocess.Popen(" ".join(args), shell=True, stdout=f2, stderr=f2, bufsize=1)
+process = subprocess.Popen(" ".join(args), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, bufsize=1)
+
+#output = process.stdout.read()
+for line in process.stdout:
+    if line is True:
+        print "<h3>The Notification was sent successfully ! </h3>"
+        print "<h2>Thanks your notification has been sent with following parameters:</h2>"
+        print "<p>TOKEN: %s</p>" % TOKEN
+        print "<p>BUNDLE ID: %s</p>" % BUNDLE_ID
+        print "<p>MESSAGE: \'%s\'</p>" % PAYLOAD
+        print "<p>CERTIFICAT: %s</p>" % CERTIFICAT
+        print "<p>COMMAND: %s</p>" % CURL_CMD
+    else:
+        print "<h3> An error occured while sending the token : \n %s </h3>" % line
+        print "<h4> Please refer to the following table to know why. \n </h4>"
+        #print "<p> See : https://developer.apple.com/library/content/documentation/NetworkingInternet/Conceptual/RemoteNotificationsPG/CommunicatingwithAPNs.html for more information on errors
+        #print "<table style=\"width:%\">"
+        with open ("../error_code.html", "r") as f:
+            for line in f:
+                print line
+
+
 #with process.stdout, open(logfile, 'ab') as file:
  #       for line in iter(process.stdout.readline, b''):
    #         print line,
